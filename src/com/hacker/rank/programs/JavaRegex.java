94c7a99772148149388c5685eac7feada7a2a596
Deepak <deepak.281295@gmail.com>
2024-01-25 16:55:18 +0530

    Java Regex

---
+++ src/com/hacker/rank/programs/JavaRegex.java
@@ -0,0 +1,23 @@
+package com.hacker.rank.programs;
+
+import java.util.Scanner;
+
+class JavaRegex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    String pattern = "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])\\." +
+            "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])\\." +
+            "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])\\." +
+            "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])"   ;
+}
+
